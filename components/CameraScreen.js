f292070380b5de859700eb12b82a3e387c4fc210
sirgentleman <imnotfreeman@gmail.com>
2021-11-05 19:56:35 +0100

    add photo button

--- components/CameraScreen.js
+++ components/CameraScreen.js
@@ -1,11 +1,53 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { View, TouchableOpacity, StyleSheet, } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 
 const CameraScreen = ({ navigation }) => {
 
     return (
-        <RNCamera style={{ flex: 1 }} captureAudio={false}/>
+        <View style={styles.container}>
+            <RNCamera style={styles.preview} captureAudio={false}/>
+            <View>
+                <TouchableOpacity style={styles.outerCapture}>
+                    <View style={styles.innerCapture}/>
+                </TouchableOpacity>
+            </View>
+        </View>
     )
 };
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: 'black',
+    },
+    preview: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    outerCapture: {
+      position: 'absolute',
+      bottom: 10,
+      borderColor: '#fff',
+      borderWidth: 2,
+      borderRadius: 100,
+      padding: 5,
+      paddingHorizontal: 5,
+      alignSelf: 'center',
+      margin: 20,
+    },
+
+    innerCapture: {
+      backgroundColor: '#fff',
+      borderWidth: 2,
+      borderRadius: 100,
+      alignSelf: 'center',
+      width: 60,
+      height: 60,
+    }
+  });
 
 export default CameraScreen;
